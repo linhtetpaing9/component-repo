aa24f3e84000c448dc193d71ff600c468a42c84a
linhtetpaing9 <linhtetpaing9@gmail.com>
2021-01-02 19:30:13 +0630

    overwrite p-12 for padding 12px coz the variant of padding in tailwind is rem must configure before use

--- components/layouts/TwoSplitLayout.tsx
+++ components/layouts/TwoSplitLayout.tsx
@@ -6,14 +6,14 @@ const TwoSplitLayout = props => {
   return (
     <Row gutter={16}>
       <Col span={8} style={{padding: 12}}>
-        <div className="overflow-y-auto overflow-x-hidden" style={{ background: '#fff', padding: 12, minHeight: 800 }}>
+        <div className="overflow-y-auto overflow-x-hidden bg-white p-12 min-h-screen">
           {
             props.left ? props.left : null
           }
         </div>
       </Col>
       <Col span={16} style={{padding: 12}}>
-        <div style={{ background: '#fff', padding: 12, minHeight: 800 }}>
+        <div className="overflow-y-auto overflow-x-hidden bg-white p-12 min-h-screen">
           {
             props.right ? props.right : null
           }
